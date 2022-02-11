1c314d2d35350f2ceaedadf99b20d75642f4f348
daniel <daniel.lin.gr@gmail.com>
2022-02-11 15:19:28 +0800

    Update digita_recognition.py

--- Tools/pattern recognition/digita_recognition.py
+++ Tools/pattern recognition/digita_recognition.py
@@ -7,3 +7,17 @@ import pytesseract
 img = Image.open('test1.png')
 text = pytesseract.image_to_string(img, lang='eng')
 print(text)
+
+
+
+'''
+1.要安裝 Tesseract 主程式
+
+  https://github.com/UB-Mannheim/tesseract/wiki
+  tesseract-ocr-w64-setup-v5.0.1.20220118.exe
+  
+2.要有TESSDATA_PREFIX 環境變數:
+
+TESSDATA_PREFIX  C:\Program Files (x86)\Tesseract-OCR\tessdata
+
+'''
