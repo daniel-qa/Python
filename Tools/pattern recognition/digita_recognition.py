ce937850c68dbdd36bac4a9b8775dbb2ac51a126
daniel <daniel.lin.gr@gmail.com>
2022-02-11 15:26:33 +0800

    Update digita_recognition.py

--- Tools/pattern recognition/digita_recognition.py
+++ Tools/pattern recognition/digita_recognition.py
@@ -3,7 +3,9 @@
 '''
 from PIL import Image
 import pytesseract
-  
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
 img = Image.open('test1.png')
 text = pytesseract.image_to_string(img, lang='eng')
 print(text)
