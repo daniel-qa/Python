d38919a6421300ba761c41c856ab2a6aa2ebf243
daniel <daniel.lin.gr@gmail.com>
2022-03-15 15:14:41 +0800

    Create pyautogui_sample.py

---
+++ Tools/pyautogui/pyautogui_sample.py
@@ -0,0 +1,20 @@
+import pyautogui
+
+# 484 , 554
+
+pyautogui.moveTo( 484 , 554 ) # Move the mouse to XY coordinates.
+
+pyautogui.click()          # Click the mouse.
+
+# 628 , 640
+
+pyautogui.moveTo( 628 , 640)   #上傳
+
+pyautogui.click()
+
+
+# 350, 50
+
+pyautogui.moveTo( 628 , 640)   # Path 
+
+pyautogui.click()
