f4de2c805a09e3005c8b7051dd899c9c26a13b6d
daniel <daniel.lin.gr@gmail.com>
2022-04-21 14:19:24 +0800

    Update retry.py

--- Tools/Retry/retry.py
+++ Tools/Retry/retry.py
@@ -4,13 +4,15 @@ from retry import retry
 ''' 重試3次，間隔2秒 '''
 @retry(ZeroDivisionError, tries=3, delay=2)
 def make_trouble():
-	'''Retry on ZeroDivisionError, raise error after 3 attempts, sleep 2 seconds between attempts.'''
-	1/0
-	print("hello")
+        '''Retry on ZeroDivisionError, raise error after 3 attempts, sleep 2 seconds between attempts.'''
+        print("hello")
+
+        #1/0
+        raise BaseException("= 自定義錯誤 =")
+        print("Success")
 
 try:
-	#1/0
-	make_trouble()
+        #1/0
+        make_trouble()
 except Exception as e:
-	print(e)
-
+        print(e)
