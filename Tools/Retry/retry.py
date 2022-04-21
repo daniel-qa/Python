aa6a4c893ecf2bb619234c1700b1b541757cad69
daniel <daniel.lin.gr@gmail.com>
2022-04-21 13:53:08 +0800

    Update retry.py

--- Tools/Retry/retry.py
+++ Tools/Retry/retry.py
@@ -1,11 +1,15 @@
-# pip install retry
+# -*- coding: utf8 -*-
 from retry import retry
 
-@retry()
-def make_trouble():
-    '''Retry until succeed'''
-    
-    
+''' 重試3次，間隔2秒 '''
 @retry(ZeroDivisionError, tries=3, delay=2)
 def make_trouble():
-    '''Retry on ZeroDivisionError, raise error after 3 attempts, sleep 2 seconds between attempts.'''    
+	'''Retry on ZeroDivisionError, raise error after 3 attempts, sleep 2 seconds between attempts.'''
+	1/0
+	print("hello")
+
+try:
+	#1/0
+	make_trouble()
+except Exception as e:
+	print(e)
