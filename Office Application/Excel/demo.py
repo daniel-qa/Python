867719d9bf37d4670089b50d37569ae3d246c4ad
daniel <daniel.lin.gr@gmail.com>
2023-01-19 15:39:39 +0800

    Update demo.py

--- Office Application/Excel/demo.py
+++ Office Application/Excel/demo.py
@@ -4,15 +4,17 @@ import os
 
 # os.chdir 是 python 切換到電腦指定路徑的方法
 os.chdir(r"D:\\")
+
 # 請填寫自己電腦裡Excel檔案的絕對路徑
 wb = openpyxl.load_workbook('produceSales.xlsx')
+
 # 請填寫要處理的Excel檔案名稱
 sheet = wb.worksheets[0]
 
 # 要更正的品名與其單價
 price_updates_dict = {'Garlic': 1.99}
 
-#使用for loop掃描所有A欄品名，如果比對一致，就進行更正與上色
+# 使用for loop掃描所有A欄品名，如果比對一致，就進行更正與上色
 print("Processing...")
 for rowNum in range(2, sheet.max_row, 1):
     produceName = sheet.cell(rowNum, 1).value
